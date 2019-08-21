490c8144dc3ddc2d6fa71cb1809bdf9fb6fb09f4
mark <mbryles@gmail.com>
2019-08-20 22:24:38 -0400

    added update and delete methods for experience, education; added get for github repos

--- models/Profile.js
+++ models/Profile.js
@@ -47,7 +47,7 @@ const ProfileSchema = new mongoose.Schema({
             },
             to: {
                 type: Date,
-                required: true
+                required: false
             },
             current: {
                 type: Boolean,
