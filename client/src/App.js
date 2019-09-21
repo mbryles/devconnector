2824a322ecc6b9bae7e3298796aec6bd52624af5
mark <mbryles@gmail.com>
2019-09-21 18:16:46 -0400

    added components and corresponding reducers for auth, alert and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import './App.css';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-  <React.Fragment>
-    <Navbar />
-    <Landing />
-  </React.Fragment>
+  <Provider store={store}>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </React.Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
